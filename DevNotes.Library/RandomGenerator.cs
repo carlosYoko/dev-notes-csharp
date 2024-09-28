025346bde8766dd39b6c8fa8b748b744ff509cac
Carlos Gimenez <carlosgibe@gmail.com>
2024-09-28 10:36:11 +0200

    random number generator

---
+++ DevNotes.Library/RandomGenerator.cs
@@ -0,0 +1,23 @@
+﻿namespace DevNotes.Library
+{
+    public class RandomGenerator : IRandomGenerator
+    {
+        public int Number { get; set; }
+        Random Random = new Random();
+
+        public RandomGenerator(int min, int max)
+        {
+            Number = RandomNumber(min, max);
+        }
+
+        public int RandomNumber(int min, int max)
+        {
+            return Random.Next(min, max);
+        }
+    }
+
+    public interface IRandomGenerator
+    {
+        int RandomNumber(int min, int max);
+    }
+}
---
+++ DevNotes.Tests/RandomGeneratorTests.cs
@@ -0,0 +1,52 @@
+﻿using DevNotes.Library;
+using Moq;
+
+namespace DevNotes.Tests
+{
+    public class RandomGeneratorTests
+    {
+        [Fact]
+        public void RandomGenerator_MockClass_Input1_5_Returns_2()
+        {
+            // Act
+            var expected = 2;
+            var randomGeneratorMock = new Mock<IRandomGenerator>();
+            randomGeneratorMock.Setup(x => x.RandomNumber(It.IsAny<int>(), It.IsAny<int>())).Returns(expected);
+
+            // Arrange
+            var result = randomGeneratorMock.Object.RandomNumber(1, 5);
+
+            // Assert
+            Assert.Equal(result, expected);
+        }
+
+        [Fact]
+        public void RandomGenerator_FakeClass_Input1_5_Returns_3()
+        {
+            // Act
+            var expected = 3;
+            var randomGeneratorFake = new RandomGeneratorFake(expected);
+
+            // Arrange
+            var result = randomGeneratorFake.Number;
+
+            // Assert
+            Assert.Equal(result, expected);
+        }
+    }
+
+    public class RandomGeneratorFake : IRandomGenerator
+    {
+        public int Number { get; set; }
+
+        public RandomGeneratorFake(int fakeResult)
+        {
+            Number = fakeResult;
+        }
+
+        public int RandomNumber(int min, int max)
+        {
+            return Number;
+        }
+    }
+}
