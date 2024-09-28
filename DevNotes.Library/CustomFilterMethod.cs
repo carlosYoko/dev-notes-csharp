2058a6287f4828af8c185875813beeddcc8dfae4
Carlos Gimenez <carlosgibe@gmail.com>
2024-09-28 10:47:19 +0200

    custom filter

---
+++ DevNotes.Library/CustomFilterMethod.cs
@@ -0,0 +1,16 @@
+﻿namespace DevNotes.Library
+{
+    public class CustomFilterMethod
+    {
+        public List<T> MyCustomFilter<T>(List<T> array, Func<T, bool> predicate)
+        {
+            List<T> list = new List<T>();
+            foreach (T item in array)
+            {
+                if (predicate(item)) list.Add(item);
+            }
+
+            return list;
+        }
+    }
+}
---
+++ DevNotes.Tests/CustomFilterMethodTests.cs
@@ -0,0 +1,23 @@
+﻿using DevNotes.Library;
+
+namespace DevNotes.Tests
+{
+    public class CustomFilterMethodTests
+    {
+        [Fact]
+        public void CustomFilterMethod_InputRandomNumbers_Returns_EvenList()
+        {
+            // Act
+            var myCustomFilter = new CustomFilterMethod();
+
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6 };
+            List<int> expected = new List<int>() { 2, 4, 6 };
+
+            // Arrange
+            var result = myCustomFilter.MyCustomFilter(list, (n) => n % 2 == 0);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
