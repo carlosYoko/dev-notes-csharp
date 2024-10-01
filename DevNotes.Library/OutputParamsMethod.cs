cecd89fe3effbe9877b598f6a2a00803e4bceb7e
Carlos Gimenez <carlosgibe@gmail.com>
2024-10-01 19:22:50 +0200

    output params in param methods

---
+++ DevNotes.Library/OutputParamsMethod.cs
@@ -0,0 +1,24 @@
+﻿namespace DevNotes.Library
+{
+    public class OutputParamsMethod : IIsEvenOrOdd
+    {
+
+        public bool IsEven(int num, out int originNumber, out int squaredNumber)
+        {
+            originNumber = num;
+            squaredNumber = num * num;
+
+            if (num % 2 == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    public interface IIsEvenOrOdd
+    {
+        bool IsEven(int num, out int originNumber, out int squaredNumber);
+    }
+}
---
+++ DevNotes.Tests/OutputParamsMethodTests.cs
@@ -0,0 +1,25 @@
+﻿using DevNotes.Library;
+
+namespace DevNotes.Tests
+{
+    public class OutputParamsMethodTests
+    {
+        [Fact]
+        void OutputParamsMethod_Input_Even_Num_Reutrns_True()
+        {
+            // Act
+            OutputParamsMethod outputParamsMethod = new OutputParamsMethod();
+            int numberForTest = 2;
+            int originNumber;
+            int squaredNumber;
+
+            // Arrange
+            bool result = outputParamsMethod.IsEven(numberForTest, out originNumber, out squaredNumber);
+
+            // Assert
+            Assert.True(result);
+            Assert.Equal(2, originNumber);
+            Assert.Equal(4, squaredNumber);
+        }
+    }
+}
