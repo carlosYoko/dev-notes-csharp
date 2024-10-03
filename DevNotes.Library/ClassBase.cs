69a4a4a5d81f819fa417de320a62c852275df64d
Carlos Gimenez <carlosgibe@gmail.com>
2024-10-03 20:57:52 +0200

    class base

---
+++ DevNotes.Library/ClassBase.cs
@@ -0,0 +1,58 @@
+﻿namespace DevNotes.Library
+{
+    public class Animal
+    {
+        public string Song { get; set; }
+        public string Name { get; set; }
+
+        public Animal(string name, string song)
+        {
+            Name = name;
+            Song = song;
+        }
+
+        public void AnimalSays()
+        {
+            Console.WriteLine($"The animal says: {Song}");
+        }
+
+        public void ShowName()
+        {
+            Console.WriteLine($"The animal's name: {Name}");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public Dog(string name, string song) : base(name, song)
+        {
+        }
+
+        public void DogSays()
+        {
+            base.AnimalSays();
+        }
+
+        public void DogName()
+        {
+            Console.WriteLine(this.Name);
+        }
+    }
+
+    public class Human : Animal
+    {
+        public Human(string name, string song) : base(name, song)
+        {
+        }
+
+        public void HumanSays()
+        {
+            base.AnimalSays();
+        }
+
+        public void HumanName()
+        {
+            Console.WriteLine(this.Name);
+        }
+    }
+}
---
+++ DevNotes.Tests/ClassBaseTests.cs
@@ -0,0 +1,43 @@
+﻿using DevNotes.Library;
+
+namespace DevNotes.Tests
+{
+    public class ClassBaseTests
+    {
+        [Fact]
+        public void DogSays_ShouldOutputCorrectMessage()
+        {
+            // Arrange
+            var dog = new Dog("Buddy", "Woof");
+            var expectedOutput = "The animal says: Woof\r\n";
+
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            // Act
+            dog.DogSays();
+
+            // Assert
+            var output = stringWriter.ToString();
+            Assert.Equal(expectedOutput, output);
+        }
+
+        [Fact]
+        public void DogName_ShouldOutputCorrectName()
+        {
+            // Arrange
+            var dog = new Dog("Buddy", "Woof");
+            var expectedOutput = "Buddy\r\n";
+
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            // Act
+            dog.DogName();
+
+            // Assert
+            var output = stringWriter.ToString();
+            Assert.Equal(expectedOutput, output);
+        }
+    }
+}
