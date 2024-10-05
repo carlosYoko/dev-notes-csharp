dec65256d806b9c8e74f0d72a069a2dcf3ed0782
Carlos Gimenez <carlosgibe@gmail.com>
2024-10-05 17:45:25 +0200

    task run

---
+++ DevNotes.Library/TaskRun.cs
@@ -0,0 +1,22 @@
+﻿namespace DevNotes.Library
+{
+    public class TaskRun
+    {
+        public async Task<int> CalculateTask()
+        {
+            return await Task.Run(() => this.CalculateAsync());
+        }
+
+        public int Calculate()
+        {
+            return 24;
+        }
+
+        private int CalculateAsync()
+        {
+            Thread.Sleep(5000);
+            return 42;
+        }
+
+    }
+}
---
+++ DevNotes.Tests/TaskRunTests.cs
@@ -0,0 +1,34 @@
+﻿using DevNotes.Library;
+
+namespace DevNotes.Tests
+{
+    public class TaskRunTests
+    {
+        [Fact]
+        public void Calculate_Returns_24()
+        {
+            // Arrange
+            var taskRun = new TaskRun();
+
+            // Act
+            var result = taskRun.Calculate();
+
+            // Assert
+            Assert.Equal(24, result);
+        }
+
+        [Fact]
+        public async Task CalculateTask_Returns_42_After_5_Seconds()
+        {
+            // Arrange
+            var taskRun = new TaskRun();
+
+            // Act
+            var task = taskRun.CalculateTask();
+
+            // Assert
+            Assert.True(await Task.WhenAny(task, Task.Delay(6000)) == task, "CalculateTask did not complete in expected time.");
+            Assert.Equal(42, await task);
+        }
+    }
+}
