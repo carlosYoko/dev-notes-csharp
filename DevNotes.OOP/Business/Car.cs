aeb7aef7fb585adb5f1007d373ebc356ba613b92
Carlos Gimenez <carlosgibe@gmail.com>
2024-10-07 21:18:07 +0200

    polimorfism

---
+++ DevNotes.OOP/Business/Car.cs
@@ -0,0 +1,16 @@
+﻿namespace DevNotes.OOP.Business
+{
+    public class Car : Vehicle
+    {
+        private const string Category = "Coche";
+
+        public Car(double horsePower) : base(horsePower)
+        {
+        }
+
+        public override string GetCategory()
+        {
+            return Category;
+        }
+    }
+}
--- DevNotes.OOP/Program.cs
+++ DevNotes.OOP/Program.cs
@@ -5,6 +5,15 @@ Bike hornetBike = new Bike("Hornet", 15000, 600, 96.5);
 var cbr6 = new InspectionBike("CBR rr", 20000, 600, 120, new DateTime(2027, 10, 08));
 
 Vehicle bandit = new Bike("Bandit", 10000, 600, 90.5);
+Show(bandit);
+
+Vehicle vehicle = new Car(250);
+Show(vehicle);
+
+vehicle = new Bike("Lightning", 25000, 600, 110);
+Show(vehicle);
+
+void Show(Vehicle vehicle) => Console.WriteLine(vehicle.GetCategory());
 
 Console.WriteLine(bandit.GetCategory());
 Console.WriteLine(bandit.GetHP());
