80a6db702ff033664c61d6c00287766ec2fbe835
Carlos Gimenez <carlosgibe@gmail.com>
2024-10-07 19:00:21 +0200

    classes and objects poo

---
+++ DevNotes.POO.ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿Bike hornetBike = new Bike()
+{
+    Name = "Hornet",
+    Price = 15000
+};
+
+var cbrBike = new Bike();
+cbrBike.Name = "CBR RR";
+cbrBike.Price = 20000;
+
+Console.WriteLine(hornetBike.Name);
+Console.WriteLine(cbrBike.Name + " " + cbrBike.Price);
+Console.WriteLine(hornetBike.GetInfo());
+
+public class Bike
+{
+    public string Name { get; set; }
+    public double Price { get; set; }
+
+    public string GetInfo()
+    {
+        return $"Modelo: {Name}, Precio: {Price}";
+    }
+}
