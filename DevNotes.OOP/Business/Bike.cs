077f717abe259855326530463959ac1aa37d4169
Carlos Gimenez <carlosgibe@gmail.com>
2024-10-07 19:35:16 +0200

    namespaces

---
+++ DevNotes.OOP/Business/Bike.cs
@@ -0,0 +1,38 @@
+﻿namespace DevNotes.OOP.Business
+{
+    public class Bike
+    {
+        private decimal _engine;
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public decimal Engine
+        {
+            get { return _engine; }
+            set
+            {
+                if (value < 0)
+                {
+                    value = 0;
+                }
+
+                _engine = value;
+            }
+        }
+        public string SEngine
+        {
+            get { return "Cilindrada: " + _engine.ToString(); }
+        }
+
+        public Bike(string name, double price, decimal engine)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.Engine = engine;
+        }
+
+        public string GetInfo()
+        {
+            return $"Modelo: {Name}, Precio: {Price}, Cilindrada: {Engine}";
+        }
+    }
+}
---
+++ DevNotes.OOP/Program.cs
@@ -0,0 +1,6 @@
+﻿using DevNotes.OOP.Business;
+
+Bike hornetBike = new Bike("Hornet", 15000, 600);
+
+Console.WriteLine(hornetBike.GetInfo());
+Console.WriteLine(hornetBike.SEngine);
