17c10e871f0dfcfc32721e9ee212df08b7b75b0a
Carlos Gimenez <carlosgibe@gmail.com>
2024-10-26 17:16:36 +0200

    delegates

--- DevNotes.Generics/Program.cs
+++ DevNotes.Generics/Program.cs
@@ -15,12 +15,32 @@ var bikeRepository = new BikeRepository();
 bikeRepository.Add("Honda");
 bikeRepository.Add("Yamaha");
 
+
+// Funciones con generics
+
 Show<string>(bikeRepository);
 
 void Show<T>(IRepository<T> repository)
 {
     foreach (var bike in repository.GetAll())
     {
+        if (bike == null) continue;
         Console.WriteLine(bike);
     }
 }
+
+
+// Delegados con generics
+
+Operation<int> add = (a, b) => a + b;
+Operation<decimal> addDouble = (a, b) => a + b;
+Operation<string> concatenate = (a, b) => $"{a} {b}";
+Action<IRepository<string>> actionDelegate = Show;
+
+
+Console.WriteLine(add(2, 5));
+Console.WriteLine(addDouble(10.2m, 5.2m));
+Console.WriteLine(concatenate("Hello", "World!"));
+actionDelegate(bikeRepository);
+
+delegate T Operation<T>(T el1, T el2);
