07824af4b762ce73399298331c16ddb3456fb196
Carlos Gimenez <carlosgibe@gmail.com>
2024-10-26 16:47:50 +0200

    basic example

--- DevNotes.Generics/Box.cs
+++ DevNotes.Generics/Box.cs
@@ -2,5 +2,16 @@
 {
     public class Box<T>
     {
+        private readonly T _value;
+
+        public Box(T value)
+        {
+            _value = value;
+        }
+
+        public string GetContent()
+        {
+            return _value.ToString();
+        }
     }
 }
--- DevNotes.Generics/Program.cs
+++ DevNotes.Generics/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿using DevNotes.Generics;
+
+var box = new Box<int>(7);
+var box2 = new Box<bool>(true);
+
+Console.WriteLine(box.GetContent());
+Console.WriteLine(box2.GetContent());
