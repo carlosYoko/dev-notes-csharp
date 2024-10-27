826d70bc3e8e7043db9718a19901e4edeb3fc6ac
Carlos Gimenez <carlosgibe@gmail.com>
2024-10-27 21:00:11 +0100

    higher order functions

--- DevNotes.FunctionalProgramming/Program.cs
+++ DevNotes.FunctionalProgramming/Program.cs
@@ -2,14 +2,15 @@
 // # Action Type #
 // ###############
 Action<string> sayHello = Console.WriteLine;
-Action<int, int> add = AddTwoNumbers;
 static void AddTwoNumbers(int a, int b)
 {
     var result = a + b;
 }
-
 sayHello("Hello World");
+
+Action<int, int> add = AddTwoNumbers;
 add(2, 2);
+
 
 // #####################
 // # Lambda expression #
@@ -18,9 +19,41 @@ Action<string> hi = name => Console.WriteLine($"Hello {name}");
 hi("Carlos");
 
 Action<string> show = Console.WriteLine;
-
 Action<int, int> addNumbers = (a, b) => show((a + b).ToString());
 addNumbers(2, 2);
+
+
+// #############
+// # Func Type #
+// #############
+Func<int, int, int> mul = (a, b) => a * b;
+show(mul(3, 4).ToString());
+
+Func<int, int, string> mulString = (a, b) =>
+ {
+     var result = a * b;
+     return result.ToString();
+ };
+show(mulString(4, 4));
+
+
+// ##########################
+// # Higher Order Functions #
+// ##########################
+List<int> list = [1, 2, 3, 4, 5, 6];
+var listFiltered = Filter(list, n => n % 2 == 0);
+var listFiltered2 = Filter(list, n => n > 3);
+
+List<int> Filter(List<int> list, Func<int, bool> condition)
+{
+    var result = new List<int>();
+    foreach (int i in list)
+    {
+        if (condition(i)) result.Add(i);
+    }
+
+    return result;
+}
 
 
 // ##################
