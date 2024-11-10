daafd0c5fd65ffbfe2dde7aa7b57b1f3844bfb1f
Carlos Gimenez <carlosgibe@gmail.com>
2024-11-10 11:55:36 +0100

    several params input in method

---
+++ DevNotes.Library/SomeMethods.cs
@@ -0,0 +1,17 @@
+﻿namespace DevNotes.Library
+{
+    public class SomeMethods
+    {
+        public double AddSeveralParams(params double[] n)
+        {
+            double result = 0;
+
+            foreach (var i in n)
+            {
+                result += i;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ DevNotes.Tests/SomeMethods.cs
@@ -0,0 +1,24 @@
+﻿using DevNotes.Library;
+
+namespace DevNotes.Tests
+{
+    public class SomeMethodsTests
+    {
+        [Fact]
+        void AddSeveralParams_Input_Several_Nums_Returns_Add()
+        {
+            // Act
+            SomeMethods someMethods = new SomeMethods();
+            double[] twoParams = { 4, 4 };
+            double[] sixParams = { 2, 2, 2, 1, 1, 1 };
+
+            // Arrange
+            var resultTwoParams = someMethods.AddSeveralParams(twoParams);
+            var resultSixParams = someMethods.AddSeveralParams(sixParams);
+
+            // Assert
+            Assert.Equal(8, resultTwoParams);
+            Assert.Equal(9, resultSixParams);
+        }
+    }
+}
